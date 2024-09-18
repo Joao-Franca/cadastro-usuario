79908ca783592449c92193340ab60d4381221ed5
Joao <joaopedro4838479@gmail.com>
2024-09-18 08:29:11 -0300

    UPDATE

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post("/cliente", async (req, res) => {
     const values = [nome, endereco, cidade, cpf, rg, telefone];
     const result = await pool.query(query, values);
 
-    res.send("Cliente cadastrado com sucesso! ID: " + result.rows[0].id);
+    res.redirect("/clientes");
   } catch (err) {
     console.error("Erro ao cadastrar cliente", err);
     res.status(500).send("Erro ao cadastrar cliente");
@@ -98,6 +98,37 @@ app.post('/login', async (req, res) => {
     res.status(500).send("Erro ao processar o login.");
   }
 })
+
+
+// Rota para deletar cliente
+app.post("/clientes/:id/delete", async (req, res) => {
+  const clienteId = req.params.id;
+  try {
+    await pool.query("DELETE FROM clientes WHERE id = $1", [clienteId]);
+    res.redirect("/clientes");  // Redireciona de volta para a lista de clientes após a exclusão
+  } catch (err) {
+    console.error("Erro ao deletar cliente", err);
+    res.status(500).send("Erro ao deletar cliente");
+  }
+});
+
+
+// Rota para atualizar o cliente
+app.post('/clientes/:id/update', async (req, res) => {
+  const clienteId = req.params.id;
+  const { nome, endereco, cpf, rg, telefone } = req.body;
+  
+  try {
+    await pool.query(
+      'UPDATE clientes SET nome = $1, endereco = $2, cpf = $3, rg = $4, telefone = $5 WHERE id = $6',
+      [nome, endereco, cpf, rg, telefone, clienteId]
+    );
+    res.redirect('/clientes'); // Redireciona para a lista de clientes após a edição
+  } catch (err) {
+    console.error('Erro ao atualizar cliente', err);
+    res.status(500).send('Erro ao atualizar cliente');
+  }
+});
 
 
 app.listen(3000, () => {
